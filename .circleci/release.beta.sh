fd14f245b881699262143667714f8913053b6a55
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2019-10-07 05:50:33 +0100

    skip-ci message in publish

--- .circleci/release.beta.sh
+++ .circleci/release.beta.sh
@@ -2,5 +2,5 @@ git tag | xargs git tag -d
 git fetch --tags
 git reset --hard HEAD
 
-npm version prerelease --preid beta && npm publish
+npm version prerelease --preid beta -m 'Publish skip-ci %s' && npm publish
 git push --tags
