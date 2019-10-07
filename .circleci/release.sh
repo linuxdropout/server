22651683683661db6c3b54100d9e9abc1a0903dc
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2019-10-07 06:32:27 +0100

    properly skip ci

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -6,5 +6,5 @@ mhash=$(git merge-base master HEAD)
 dirpath=$(dirname $0)
 version=$(git log $mhash.. | grep -v 'commit\|^Author:\|^Date:' | node $dirpath/version.js)
 
-npm version $version -m 'Publish skip ci %s' && npm publish
+npm version $version -m 'Publish [skip ci] %s' && npm publish
 git push && git push --tags
