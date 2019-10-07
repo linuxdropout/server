71200789540b670d768768f04a1420e440d2dec8
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2019-10-07 06:34:40 +0100

    only push when successful

--- .circleci/release.beta.sh
+++ .circleci/release.beta.sh
@@ -2,5 +2,4 @@ git tag | xargs git tag -d
 git fetch --tags
 git reset --hard HEAD
 
-npm version prerelease --preid beta -m 'Publish [skip ci] %s' && npm publish
-git push && git push --tags
+npm version prerelease --preid beta -m 'Publish [skip ci] %s' && npm publish && git push && git push --tags
